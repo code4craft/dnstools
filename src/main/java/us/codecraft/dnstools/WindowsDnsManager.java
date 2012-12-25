82782cfb691cdfb68da3a9308e4573cb5a8c0c4a
flashsword20 <flashsword20@gmail.com>
2012-12-25 11:46:40 +0800

    init

---
+++ src/main/java/us/codecraft/dnstools/WindowsDnsManager.java
@@ -0,0 +1,71 @@
+package us.codecraft.dnstools;
+
+import java.util.List;
+
+/**
+ * @author yihua.huang@dianping.com
+ * @date Dec 25, 2012
+ */
+public class WindowsDnsManager implements DnsManager {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see us.codecraft.dnstools.DnsManager#getDNS()
+	 */
+	public List<String> getDNS() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see us.codecraft.dnstools.DnsManager#setDNS(java.util.List)
+	 */
+	public void setDNS(List<String> dns) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see us.codecraft.dnstools.DnsManager#getIp()
+	 */
+	public String getIp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see us.codecraft.dnstools.DnsManager#setIp()
+	 */
+	public void setIp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see us.codecraft.dnstools.DnsManager#getGateway()
+	 */
+	public String getGateway() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see us.codecraft.dnstools.DnsManager#setGateway()
+	 */
+	public void setGateway() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
